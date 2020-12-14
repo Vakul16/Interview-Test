f67aaf197147ca626742286025649abad9e33677
Vakul16 <valusavakhul@gmail.com>
2020-12-14 13:12:55 +0530

    files added

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './layout.css'
+const Header = () => {
+    return (
+        <div class = "header">
+            <h1> Header</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './layout.css'
+
+
+const Main = () => {
+    return (
+        <div class = "main">
+            <h1> Main</h1>
+        </div>
+    )
+}
+export default Main
+// kranthi@dhan.ai
